088b8f120ae127e7a5aa80b6bde5734c0951bf50
takemura-hyd <113428411+takemura-hyd@users.noreply.github.com>
2022-10-10 18:37:43 +0900

    Delete mingw-w64.bat

--- 2.5-FlowResistanceInCompoundChannels/mingw-w64.bat
+++
@@ -1,5 +0,0 @@
-echo off
-set PATH=C:\Program Files\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin;%PATH%
-rem echo %PATH%
-rem cd "C:\Program Files\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin"
-"C:\WINDOWS\system32\cmd.exe"
